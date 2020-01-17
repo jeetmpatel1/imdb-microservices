f63173baec79edbfea873611d4831a289c5357f0
jeetmpatel1 <jeet.mpatel1@gmail.com>
2020-01-16 17:06:58 -0800

    m3 ratings-data-service added

---
+++ ratings-data-service/src/main/java/com/imdb/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,34 @@
+    package com.imdb.ratingsdataservice.resources;
+
+
+    import com.imdb.ratingsdataservice.models.Rating;
+    import com.imdb.ratingsdataservice.models.UserRating;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
+
+    import java.util.Arrays;
+    import java.util.List;
+
+    @RestController
+    @RequestMapping("/ratingsdata")
+    public class RatingsResource {
+
+        @RequestMapping("/{movieId}")
+        public Rating getRating(@PathVariable("movieId") String movieId){
+            return new Rating(movieId,4);
+        }
+
+        @RequestMapping("users/{userId}")
+        public UserRating getUserRating(@PathVariable("userId") String userId){
+            List<Rating> ratings = Arrays.asList(
+                    new Rating("1",4),
+                    new Rating("2",3),
+                    new Rating("3",5)
+            );
+            UserRating userRating = new UserRating();
+            userRating.setUserRating(ratings);
+            return userRating;
+        }
+
+    }
